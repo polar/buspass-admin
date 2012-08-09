d2f7280e4a906346bf0702e2803a5a89c4551909
polar <polar@adiron.com>
2012-08-09 17:16:34 -0400

    Progress

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -4,7 +4,7 @@ require 'mongo'
 if ENV['MONGOLAB_URI']
   uri  = URI.parse(ENV['MONGOLAB_URI'])
   MongoMapper.connection = conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-  db = conn.db(uri.path.gsub(/^\//, '')) 
+  MongoMapper.database = (uri.path.gsub(/^\//, ''))
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
   MongoMapper.database = "#Busme-#{Rails.env}"
