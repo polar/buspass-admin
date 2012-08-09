5bdb8b767cd3053cea96f4ff29185b237bb8324b
polar <polar@adiron.com>
2012-08-09 16:53:37 -0400

    Progress

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -4,11 +4,12 @@ require 'mongo'
 if ENV['MONGOLAB_URI']
   uri  = URI.parse(ENV['MONGOLAB_URI'])
   MongoMapper.connection = conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  db = conn.db(uri.path.gsub(/^\//, '')) 
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
+  MongoMapper.database = "#Busme-#{Rails.env}"
 end
 
-MongoMapper.database = conn.db(uri.path.gsub(/^\//, '')) # "#Busme-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
