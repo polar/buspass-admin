bcf33bc6c0dcf2217924b399d3f263badb5f0cc1
polar <polar@adiron.com>
2012-08-08 21:01:23 -0400

    Progress

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,13 +20,13 @@ after "deploy:restart", "deploy:cleanup"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
 
 set :rvm_ruby_string, 'ruby-1.9.3-p125@rails3.2'
