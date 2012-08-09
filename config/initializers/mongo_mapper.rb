09521bc166276724b19dee4129ff400d5fdeac47
polar <polar@adiron.com>
2012-08-09 16:48:13 -0400

    Progress

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -8,7 +8,7 @@ else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
 end
 
-MongoMapper.database = "#Busme-#{Rails.env}"
+MongoMapper.database = conn.db(uri.path.gsub(/^\//, '')) # "#Busme-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
