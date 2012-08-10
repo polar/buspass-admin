d00679b367c0a018dea85d46db5bff88c0cd8405
polar <polar@adiron.com>
2012-08-10 11:22:29 -0400

    Progress

--- app/models/google_uri_view_path.rb
+++ app/models/google_uri_view_path.rb
@@ -32,17 +32,18 @@ class GoogleUriViewPath
         #puts "no cache item, getting from Internet"
         doc = open(uri) { |f| Hpricot(f) }
         if doc
-          coord_html = doc.at("geometrycollection/linestring/coordinates")
-          if coord_html
-            if (uri.start_with?("http://google"))
-              x = coord_html.inner_html.split(",0.000000 ").map { |x| eval "[#{x}]" }
-            else
-              x = coord_html.inner_html.(" ").map { |x| eval "[#{x}]" }
-            end
-            ans                         = {"LonLat" => x}
-            cache.view_path_coordinates = ans
-            cache.save!
+          if (uri.start_with?("http://google"))
+            coord_html = doc.at("geometrycollection/linestring/coordinates")
+            x = coord_html.inner_html.split(",0.000000 ").map { |x| eval "[#{x}]" }
+          elsif (uri.start_with?("http://www.yournavigation.org"))
+            coord_html = doc.at("document/folder/placemark/linestring/coordinates")
+            x = coord_html.inner_html.(" ").map { |x| eval "[#{x}]" }
+          else
+            x = [[0,0],[1,1]] # bogus
           end
+          ans                         = {"LonLat" => x}
+          cache.view_path_coordinates = ans
+          cache.save!
         end
       else
         ans = cache.view_path_coordinates
