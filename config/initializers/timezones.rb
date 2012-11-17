600f04d7cea65ac45fefd8f77e376de0cb6b4bd3
Polar Humenn <polar@adiron.com>
2012-11-17 18:21:31 -0500

    Progress.

--- config/initializers/timezones.rb
+++ config/initializers/timezones.rb
@@ -6,8 +6,16 @@ puts "TimeZones Start"
 
 require "timezone"
 
+puts "Done loading Timezone."
+
 # List for Views selections.
 
-TIMEZONES_LIST = Timezone::Zone.list.map {|z| z[:title] }.sort
+TIMEZONES_LIST = Timezone::Zone.names
+
+puts "Got TZ names #{TIMEZONES_LIST.count}"
+
+TIMEZONES_LIST = Timezone::Zone.list.map { |z| z[:title] }.sort
+
+puts "Got TZ list #{TIMEZONES_LIST.count}"
 
 puts "TimeZones Done"
