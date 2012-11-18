437125de216459f7ac0a5effcce963bab812af4a
Polar Humenn <polar@adiron.com>
2012-11-18 11:52:13 -0500

    Progress.

--- app/assets/javascripts/webmap.js
+++ app/assets/javascripts/webmap.js
@@ -5,3 +5,8 @@
 //= require OpenLayers-2.11/OpenLayers
 //= require BusPass
 //= require ActivePlanBasketController
+//= require_self
+
+// Compiling the assets into a single file makes OpenLayers._getScriptLocation to be wrong
+// and therefore cannot find the images. We configure the OpenLayers image prefix here.
+OpenLayers.ImgPath = "/assets/OpenLayers-2.11/img/"
